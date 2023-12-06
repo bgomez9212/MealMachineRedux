49608751d6b0d5944ed5f25948bd01f9065b6210
Brandon Gomez <bgomez9212@gmail.com>
2023-12-05 16:13:08 -0800

    ingredient template created

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -1,11 +1,12 @@
 import { RecipeCard } from "@/components/RecipeCard";
 import data from "./randomRecipes.json";
+
 export function Home() {
   const recipes = data.recipes;
   return (
     <div className="px-10 flex flex-wrap justify-between">
-      {recipes.map((recipe) => (
-        <RecipeCard recipe={recipe} />
+      {recipes.map(({ title, image }) => (
+        <RecipeCard key={title} title={title} image={image} />
       ))}
     </div>
   );
--- app/src/pages/myrecipes.tsx
+++ app/src/pages/myrecipes.tsx
@@ -1,11 +1,5 @@
 import { RecipeCard } from "@/components/RecipeCard";
 
 export function MyRecipes() {
-  return (
-    <div className="px-10 flex justify-between">
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-    </div>
-  );
+  return <div className="px-10 flex justify-between"></div>;
 }
