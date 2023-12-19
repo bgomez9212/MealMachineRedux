87b73e19377d47f9c02606dde286742be5a99c40
Brandon Gomez <bgomez9212@gmail.com>
2023-12-18 17:18:09 -0800

    routers/controllers/models/queries added for ingredients and groceries

--- server/controller.js
+++ server/controller.js
@@ -1,18 +1,38 @@
 const model = require("./model.js");
 
 module.exports = {
-  getFood: async (req, res) => {
-    const results = await model.getFood();
-    res.send(results.rows).status(200);
+  getSavedRecipes: async (req, res) => {
+    const result = await model.getSavedRecipes(req.query.user_id);
+    res.send(result.rows).status(200);
   },
-  postRecipe: async (req, res) => {
-    const { user_id, recipeId } = req.query;
-    await model.postSavedRecipe(user_id, recipeId);
+  getIngredients: async (req, res) => {
+    const result = await model.getIngredients(req.query.user_id);
+    res.send(result.rows).status(200);
+  },
+  postIngredients: async (req, res) => {
+    const result = await model.postIngredients(
+      req.query.user_id,
+      req.query.food_name
+    );
     res.sendStatus(201);
   },
-  getSavedRecipes: async (req, res) => {
-    const { user_id } = req.query;
-    const result = await model.getSavedRecipes(user_id);
-    res.send(results.rows).status(200);
+  deleteIngredients: async (req, res) => {
+    const result = await model.deleteIngredients(req.query.ing_user_id);
+    res.sendStatus(204);
+  },
+  getGroceries: async (req, res) => {
+    const result = await model.getGroceries(req.query.user_id);
+    res.send(result.rows).status(200);
+  },
+  postGroceries: async (req, res) => {
+    const result = await model.postGroceries(
+      req.query.user_id,
+      req.query.food_name
+    );
+    res.sendStatus(201);
+  },
+  deleteGroceries: async (req, res) => {
+    const result = await model.deleteGroceries(req.query.gro_user_id);
+    res.sendStatus(204);
   },
 };
--- server/model.js
+++ server/model.js
@@ -1,19 +1,127 @@
 const pool = require("../db/index.js");
 
 module.exports = {
-  getFood: async (user_id) => {
-    const query = "SELECT * FROM food";
-    const result = await pool.query(query);
-    return result;
-  },
-  postSavedRecipe: async (user_id, recipeId) => {
-    const query = "INSERT INTO savedRecipes VALUES ($1, $2)";
-    const result = await pool.query(query, [user_id, recipeId]);
-    return result;
-  },
+  // get saved recipes for user
   getSavedRecipes: async (user_id) => {
-    const query = "SELECT * FROM savedRecipes WHERE user_id = $1";
+    const query = `SELECT * FROM savedRecipes WHERE user_id = $1`;
     const result = await pool.query(query, [user_id]);
     return result;
+  },
+  // get ingredients for user
+  getIngredients: async (user_id) => {
+    const query =
+      "SELECT name FROM food INNER JOIN ingredients ON food.id = ingredients.food_id WHERE ingredients.user_id = $1";
+    const result = await pool.query(query, [user_id]);
+    return result;
+  },
+  // add ingredient for user, also adding name to food table if name does not exist
+  postIngredients: async (user_id, food_name) => {
+    const client = await pool.connect();
+
+    try {
+      // Start a transaction
+      await client.query("BEGIN");
+
+      // Check if the food exists
+      const checkFoodQuery = "SELECT id FROM food WHERE name = $1";
+      const foodResult = await client.query(checkFoodQuery, [food_name]);
+
+      let food_id;
+
+      if (foodResult.rows.length === 0) {
+        // If the food does not exist, insert it into the food table
+        const insertFoodQuery =
+          "INSERT INTO food (name) VALUES ($1) RETURNING id";
+        const insertFoodResult = await client.query(insertFoodQuery, [
+          food_name,
+        ]);
+        food_id = insertFoodResult.rows[0].id;
+      } else {
+        // If the food already exists, use its id
+        food_id = foodResult.rows[0].id;
+      }
+
+      // Insert into the ingredients table, referencing the food table
+      const insertIngredientQuery =
+        "INSERT INTO ingredients (user_id, food_id, ing_user_id) VALUES ($1, $2, $3) ON CONFLICT (ing_user_id) DO NOTHING";
+      await client.query(insertIngredientQuery, [
+        user_id,
+        food_id,
+        food_id + user_id,
+      ]);
+
+      // Commit the transaction
+      await client.query("COMMIT");
+    } catch (err) {
+      // If any error occurs, rollback the transaction
+      await client.query("ROLLBACK");
+      throw err;
+    } finally {
+      // Release the client back to the pool
+      client.release();
+    }
+  },
+  // remove ingredient for user
+  deleteIngredients: async (ing_user_id) => {
+    const query = "DELETE FROM ingredients WHERE ing_user_id = $1";
+    await pool.query(query, [ing_user_id]);
+  },
+  // get groceries for user
+  getGroceries: async (user_id) => {
+    const query =
+      "SELECT * FROM food INNER JOIN groceries ON food.id = groceries.food_id WHERE groceries.user_id = $1";
+    const result = await pool.query(query, [user_id]);
+    return result;
+  },
+  // add groceries for user
+  postGroceries: async (user_id, food_name) => {
+    const client = await pool.connect();
+
+    try {
+      // Start a transaction
+      await client.query("BEGIN");
+
+      // Check if the food exists
+      const checkFoodQuery = "SELECT id FROM food WHERE name = $1";
+      const foodResult = await client.query(checkFoodQuery, [food_name]);
+
+      let food_id;
+
+      if (foodResult.rows.length === 0) {
+        // If the food does not exist, insert it into the food table
+        const insertFoodQuery =
+          "INSERT INTO food (name) VALUES ($1) RETURNING id";
+        const insertFoodResult = await client.query(insertFoodQuery, [
+          food_name,
+        ]);
+        food_id = insertFoodResult.rows[0].id;
+      } else {
+        // If the food already exists, use its id
+        food_id = foodResult.rows[0].id;
+      }
+
+      // Insert into the groceries table, referencing the food table
+      const insertGroceryQuery =
+        "INSERT INTO groceries (user_id, food_id, gro_user_id) VALUES ($1, $2, $3) ON CONFLICT (gro_user_id) DO NOTHING";
+      await client.query(insertGroceryQuery, [
+        user_id,
+        food_id,
+        food_id + user_id,
+      ]);
+
+      // Commit the transaction
+      await client.query("COMMIT");
+    } catch (err) {
+      // If any error occurs, rollback the transaction
+      await client.query("ROLLBACK");
+      throw err;
+    } finally {
+      // Release the client back to the pool
+      client.release();
+    }
+  },
+  deleteGroceries: async (gro_user_id) => {
+    const query = "DELETE FROM groceries WHERE gro_user_id = $1";
+    await pool.query(query, [gro_user_id]);
   },
 };
