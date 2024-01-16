a3d954f0a2fcf7ee0ee5f6a9ff6cd2f1d85e4d77
Brandon Gomez <bgomez9212@gmail.com>
2024-01-15 19:26:10 -0800

    changed server to fit railway's recs

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 8888;
 const db = require("./db.js");
 const router = require("./router.js");
 
@@ -10,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
