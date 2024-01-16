cbfb9d4080ca3df216b108a20dd7cc73cc0f03e7
Brandon Gomez <bgomez9212@gmail.com>
2024-01-16 12:37:17 -0800

    changed env example and smaller image size for landing page

--- app/src/pages/LandingPage.tsx
+++ app/src/pages/LandingPage.tsx
@@ -12,7 +12,7 @@ export function LandingPage({ authenticateUser }: landingpageprops) {
     setHasAccount(!hasAccount);
   };
   return (
-    <div className="bg-[url('landing-page.jpg')] h-screen w-screen bg-cover flex justify-center items-center">
+    <div className="bg-[url('/landing-page.jpg')] h-screen w-screen bg-cover flex justify-center items-center">
       <div className="bg-[#fcfcf6] dark:bg-[#526345] h-[75vh] w-[50vw] flex justify-center items-center shadow-2xl min-w-[350px]">
         {!hasAccount && (
           <Signup
