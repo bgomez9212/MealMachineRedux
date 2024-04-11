b5defe370c3a292b599d84558dd6daf495e5c886
Brandon Gomez <bgomez9212@gmail.com>
2024-04-10 20:32:42 -0700

    app.tsx test. -whole app ~36% covered

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,15 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 describe("App component", () => {
   it("renders app component", () => {
     render(<App />);
     expect(screen.getByTestId("app-loader")).toBeInTheDocument();
   });
-  // it("navigates to landing page without no user auth", async () => {
-  //   render(<App />);
-  //   await waitFor(() =>
-  //     expect(screen.getByTestId("landing-page")).toBeInTheDocument()
-  //   );
-  // });
+  it("navigates to landing page without no user auth", async () => {
+    const queryClient = new QueryClient();
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    );
+    await waitFor(() =>
+      expect(screen.getByTestId("login-component")).toBeInTheDocument()
+    );
+  });
 });
