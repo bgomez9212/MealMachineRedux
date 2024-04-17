e590e720ab242474ca4f327b0a00e5316eed6f35
Brandon Gomez <bgomez9212@gmail.com>
2024-04-16 17:50:14 -0700

    groceries page 95 percent covered

--- app/src/pages/Groceries.test.tsx
+++ app/src/pages/Groceries.test.tsx
@@ -3,25 +3,14 @@ import { MyGroceries } from "./Groceries";
 import { QueryClient, QueryClientProvider } from "react-query";
 import UserContextProvider from "@/context/context";
 import { server } from "@/mocks/browser";
+import { http, HttpResponse } from "msw";
 
 describe("MyGroceries page", () => {
-  server.listen();
-  const queryClient = new QueryClient();
-  it("calls delete and move grocery functions", async () => {
-    render(
-      <UserContextProvider testUser="test-user">
-        <QueryClientProvider client={queryClient}>
-          <MyGroceries />
-        </QueryClientProvider>
-      </UserContextProvider>
-    );
-    await waitFor(() => {
-      expect(screen.getByTestId("groceries-container")).toBeInTheDocument();
-    });
-    fireEvent.click(screen.getByTestId("remove-grocery"));
-    fireEvent.click(screen.getByTestId("move-grocery"));
-  });
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
+  const queryClient = new QueryClient();
   it("calls delete and move grocery functions", async () => {
     render(
       <UserContextProvider testUser="test-user">
@@ -50,5 +39,28 @@ describe("MyGroceries page", () => {
     ) as HTMLInputElement;
     fireEvent.change(textInput, { target: { value: "carrots, potatoes" } });
     fireEvent.click(screen.getByTestId("input-submit-button"));
+  });
+  it("renders empty array message", async () => {
+    server.use(
+      http.get(
+        import.meta.env.VITE_server_groceries,
+        () => {
+          return HttpResponse.json([]);
+        },
+        {
+          once: true,
+        }
+      )
+    );
+    render(
+      <UserContextProvider testUser="test-user">
+        <QueryClientProvider client={queryClient}>
+          <MyGroceries />
+        </QueryClientProvider>
+      </UserContextProvider>
+    );
+    await waitFor(() => {
+      expect(screen.getByTestId("no-groceries-message")).toBeInTheDocument();
+    });
   });
 });
