eb17175dcde3dd7e764ad0856a52e33b6d0df6d1
Brandon Gomez <bgomez9212@gmail.com>
2024-06-04 14:59:45 -0700

    mock database error in saved recipes/ingredients/groceries

--- server/endToEnd.test.js
+++ server/endToEnd.test.js
@@ -6,6 +6,13 @@ afterAll(async () => {
   await pool.end();
 });
 
+// simulate a database error
+function mockDb() {
+  return jest.spyOn(pool, "query").mockImplementationOnce(() => {
+    throw new Error("Database error");
+  });
+}
+
 describe("test saved recipes route", () => {
   it("should throw an error with 404 status code if a user_id is not passed to get", async () => {
     const res = await request(app).get("/api/savedRecipes");
@@ -24,13 +31,19 @@ describe("test saved recipes route", () => {
     expect(res.status).toBe(404);
   });
 
-  it("should throw an error if there is a database error", async () => {
-    const mockQuery = jest.spyOn(pool, "query").mockImplementationOnce(() => {
-      throw new Error("Database error");
-    });
+  it("should throw an error if there is a database error getting recipes", async () => {
+    mockDb();
     const res = await request(app)
       .get("/api/savedRecipes")
       .query({ user_id: 12345 });
+    expect(res.status).toBe(404);
+  });
+
+  it("should throw an error if there is a database error deleting recipes", async () => {
+    mockDb();
+    const res = await request(app)
+      .delete("/api/savedRecipes")
+      .send({ user_id: 12345, recipe_id: 12 });
     expect(res.status).toBe(404);
   });
 
@@ -74,148 +87,196 @@ describe("test saved recipes route", () => {
   });
 });
 
-// describe("test ingredients route", () => {
-//   it("should return an error when missing user_id in params for get", async () => {
-//     const res = await request(app).get("/api/ingredients");
-//     expect(res.status).toBe(404);
-//   });
+describe("test ingredients route", () => {
+  it("should return an error when missing user_id in params for get", async () => {
+    const res = await request(app).get("/api/ingredients");
+    expect(res.status).toBe(404);
+  });
 
-//   it("should return a 201 status when posting an ingredients", async () => {
-//     const res = await request(app)
-//       .post("/api/ingredients")
-//       .send({ user_id: 12345, food_name: "onions" });
-//     expect(res.status).toBe(201);
-//   });
+  it("should return an error when there is a database error getting ingredients", async () => {
+    mockDb();
+    const res = await request(app)
+      .get("/api/ingredients")
+      .query({ user_id: 12345 });
+    expect(res.status).toBe(404);
+  });
 
-//   it("should return status 200, user_id, and food_id when getting ingredients", async () => {
-//     const { rows: foodId } = await pool.query(
-//       `SELECT id FROM food WHERE name = $1`,
-//       ["onions"]
-//     );
-//     const res = await request(app)
-//       .get("/api/ingredients")
-//       .query({ user_id: 12345 });
-//     expect(res.body[0].user_id).toBe("12345");
-//     expect(res.body[0].food_id).toBe(foodId[0].id);
-//     expect(res.status).toBe(200);
-//   });
+  it("should return a 404 status when missing a parameter in a post", async () => {
+    const res = await request(app)
+      .post("/api/ingredients")
+      .send({ food_name: "carrots" });
+    expect(res.status).toBe(404);
+  });
 
-//   it("should return a 404 status when missing a parameter in a post", async () => {
-//     const res = await request(app)
-//       .post("/api/ingredients")
-//       .send({ food_name: "carrots" });
-//     expect(res.status).toBe(404);
-//   });
+  it("should return an error when there is a database while posting ingredients", async () => {
+    mockDb();
+    const res = await request(app)
+      .post("/api/ingredients")
+      .send({ user_id: 12345, food_name: "steak" });
+    expect(res.status).toBe(404);
+  });
 
-//   it("should delete an ingredient", async () => {
-//     const { rows: ingredientId } = await pool.query(
-//       `SELECT id FROM ingredients WHERE user_id = $1 AND food_id = (SELECT id FROM food WHERE name = $2)`,
-//       ["12345", "onions"]
-//     );
-//     const res = await request(app)
-//       .delete("/api/ingredients")
-//       .send({ ingredient_id: ingredientId[0].id });
-//     expect(res.status).toBe(204);
-//   });
+  it("should send an error when ingredient id is missing in delete", async () => {
+    const res = await request(app).delete("/api/ingredients");
+    expect(res.status).toBe(404);
+  });
 
-//   it("should send an error when ingredient id is missing in delete", async () => {
-//     const res = await request(app).delete("/api/ingredients");
-//     expect(res.status).toBe(404);
-//   });
-// });
+  it("should throw an error when there is a database error deleting ingredient", async () => {
+    mockDb();
+    const res = await request(app)
+      .delete("/api/ingredients")
+      .send({ ingredient_id: 1 });
+    expect(res.status).toBe(404);
+  });
 
-// describe("test groceries route", () => {
-//   it("should return an error when missing user_id in params for get", async () => {
-//     const res = await request(app).get("/api/groceries");
-//     expect(res.status).toBe(404);
-//   });
+  it("should return a 201 status when posting an ingredients", async () => {
+    const res = await request(app)
+      .post("/api/ingredients")
+      .send({ user_id: 12345, food_name: "onions" });
+    expect(res.status).toBe(201);
+  });
 
-//   it("should return a 201 status when posting an groceries", async () => {
-//     const res = await request(app)
-//       .post("/api/groceries")
-//       .send({ user_id: 12345, food_name: "onions" });
-//     expect(res.status).toBe(201);
-//   });
+  it("should return status 200, user_id, and food_id when getting ingredients", async () => {
+    const { rows: foodId } = await pool.query(
+      `SELECT id FROM food WHERE name = $1`,
+      ["onions"]
+    );
+    const res = await request(app)
+      .get("/api/ingredients")
+      .query({ user_id: 12345 });
+    expect(res.body[0].user_id).toBe("12345");
+    expect(res.body[0].food_id).toBe(foodId[0].id);
+    expect(res.status).toBe(200);
+  });
 
-//   it("should return status 200 when getting groceries", async () => {
-//     const { rows: foodId } = await pool.query(
-//       `SELECT id FROM food WHERE name = $1`,
-//       ["onions"]
-//     );
-//     const res = await request(app)
-//       .get("/api/groceries")
-//       .query({ user_id: 12345 });
-//     expect(res.status).toBe(200);
-//     expect(res.body[0].user_id).toBe("12345");
-//     expect(res.body[0].food_id).toBe(foodId[0].id);
-//   });
+  it("should delete an ingredient", async () => {
+    const { rows: ingredientId } = await pool.query(
+      `SELECT id FROM ingredients WHERE user_id = $1 AND food_id = (SELECT id FROM food WHERE name = $2)`,
+      ["12345", "onions"]
+    );
+    const res = await request(app)
+      .delete("/api/ingredients")
+      .send({ ingredient_id: ingredientId[0].id });
+    expect(res.status).toBe(204);
+  });
+});
 
-//   it("should return a 404 status when missing a parameter in post", async () => {
-//     const res = await request(app)
-//       .post("/api/groceries")
-//       .send({ food_name: "carrots" });
-//     expect(res.status).toBe(404);
-//   });
+describe("test groceries route", () => {
+  it("should return an error when missing user_id in params for get", async () => {
+    const res = await request(app).get("/api/groceries");
+    expect(res.status).toBe(404);
+  });
 
-//   it("should delete an ingredient", async () => {
-//     const { rows: groceryId } = await pool.query(
-//       `SELECT id FROM groceries WHERE user_id = $1 AND food_id = (SELECT id FROM food WHERE name = $2)`,
-//       ["12345", "onions"]
-//     );
-//     const res = await request(app)
-//       .delete("/api/groceries")
-//       .send({ grocery_id: groceryId[0].id });
-//     expect(res.status).toBe(204);
-//   });
+  it("should throw an error when there is a database error: get", async () => {
+    mockDb();
+    const res = await request(app)
+      .get("/api/groceries")
+      .query({ user_id: 12345 });
+    expect(res.status).toBe(404);
+  });
 
-//   it("should send an error when ingredient id is missing in delete", async () => {
-//     const res = await request(app).delete("/api/groceries");
-//     expect(res.status).toBe(404);
-//   });
-// });
+  it("should return a 404 status when missing a parameter in post", async () => {
+    const res = await request(app)
+      .post("/api/groceries")
+      .send({ food_name: "carrots" });
+    expect(res.status).toBe(404);
+  });
 
-// describe("test get recipes", () => {
-//   it("should return an error when a user_id is not passed to get", async () => {
-//     const res = await request(app).get("/api/recipes");
-//     expect(res.status).toBe(404);
-//   });
+  it("should throw an error when there is a database error: post", async () => {
+    mockDb();
+    const res = await request(app)
+      .post("/api/groceries")
+      .send({ user_id: 12345, food_name: "steak" });
+    expect(res.status).toBe(404);
+  });
 
-//   it("should return an empty array and status code 200", async () => {
-//     const res = await request(app).get("/api/recipes").query({ user_id: 1 });
-//     expect(res.status).toBe(200);
-//     expect(res.body).toStrictEqual([]);
-//   });
-// });
+  it("should send an error when ingredient id is missing in delete", async () => {
+    const res = await request(app).delete("/api/groceries");
+    expect(res.status).toBe(404);
+  });
 
-// describe("recipe details tests", () => {
-//   it("returns an error when there is no recipe id", async () => {
-//     const res = await request(app).get("/api/recipeDetails");
-//     expect(res.status).toBe(404);
-//   });
+  it("should throw an error when there is a database error: delete", async () => {
+    mockDb();
+    const res = await request(app)
+      .delete("/api/groceries")
+      .send({ grocery_id: 123 });
+    expect(res.status).toBe(404);
+  });
 
-//   it("returns a recipe object when an existing recipe id is passed", async () => {
-//     const res = await request(app)
-//       .get("/api/recipeDetails")
-//       .query({ recipe_id: 716429 });
-//     expect(res.status).toBe(200);
-//     expect(res.body.id).toBe(716429);
-//     expect(res.body.title).toBe(
-//       "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
-//     );
-//   });
-// });
+  it("should return a 201 status when posting an groceries", async () => {
+    const res = await request(app)
+      .post("/api/groceries")
+      .send({ user_id: 12345, food_name: "onions" });
+    expect(res.status).toBe(201);
+  });
 
-// describe("test search recipes", () => {
-//   it("returns an error if search is missing parameters", async () => {
-//     const res = await request(app).get("/api/searchRecipes");
-//     expect(res.status).toBe(404);
-//   });
+  it("should return status 200 when getting groceries", async () => {
+    const { rows: foodId } = await pool.query(
+      `SELECT id FROM food WHERE name = $1`,
+      ["onions"]
+    );
+    const res = await request(app)
+      .get("/api/groceries")
+      .query({ user_id: 12345 });
+    expect(res.status).toBe(200);
+    expect(res.body[0].user_id).toBe("12345");
+    expect(res.body[0].food_id).toBe(foodId[0].id);
+  });
 
-//   it("returns a list of recipes when passing correct parameters", async () => {
-//     const res = await request(app)
-//       .get("/api/searchRecipes")
-//       .query({ user_id: 12345, term: "pasta" });
-//     expect(res.status).toBe(200);
-//     expect(res.body.results.length).toBe(9);
-//   });
-// });
+  it("should delete an ingredient", async () => {
+    const { rows: groceryId } = await pool.query(
+      `SELECT id FROM groceries WHERE user_id = $1 AND food_id = (SELECT id FROM food WHERE name = $2)`,
+      ["12345", "onions"]
+    );
+    const res = await request(app)
+      .delete("/api/groceries")
+      .send({ grocery_id: groceryId[0].id });
+    expect(res.status).toBe(204);
+  });
+});
+
+describe("test get recipes", () => {
+  it("should return an error when a user_id is not passed to get", async () => {
+    const res = await request(app).get("/api/recipes");
+    expect(res.status).toBe(404);
+  });
+
+  it("should return an empty array and status code 200", async () => {
+    const res = await request(app).get("/api/recipes").query({ user_id: 1 });
+    expect(res.status).toBe(200);
+    expect(res.body).toStrictEqual([]);
+  });
+});
+
+describe("recipe details tests", () => {
+  it("returns an error when there is no recipe id", async () => {
+    const res = await request(app).get("/api/recipeDetails");
+    expect(res.status).toBe(404);
+  });
+
+  it("returns a recipe object when an existing recipe id is passed", async () => {
+    const res = await request(app)
+      .get("/api/recipeDetails")
+      .query({ recipe_id: 716429 });
+    expect(res.status).toBe(200);
+    expect(res.body.id).toBe(716429);
+    expect(res.body.title).toBe(
+      "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
+    );
+  });
+});
+
+describe("test search recipes", () => {
+  it("returns an error if search is missing parameters", async () => {
+    const res = await request(app).get("/api/searchRecipes");
+    expect(res.status).toBe(404);
+  });
+
+  it("returns a list of recipes when passing correct parameters", async () => {
+    const res = await request(app)
+      .get("/api/searchRecipes")
+      .query({ user_id: 12345, term: "pasta" });
+    expect(res.status).toBe(200);
+    expect(res.body.results.length).toBe(9);
+  });
+});
