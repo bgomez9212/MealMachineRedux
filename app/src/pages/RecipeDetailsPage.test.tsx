5cb56cb886c046948f7c3b7579db69a440f8f497
Brandon Gomez <bgomez9212@gmail.com>
2024-06-06 16:54:56 -0700

    correct tests for details

--- app/src/pages/RecipeDetailsPage.test.tsx
+++ app/src/pages/RecipeDetailsPage.test.tsx
@@ -3,6 +3,8 @@ import { RecipeDetailPage } from "./RecipeDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router } from "react-router-dom";
 import { server } from "@/mocks/browser";
+import UserContextProvider from "@/context/context";
+import { HttpResponse, http } from "msw";
 
 describe("recipe details page", async () => {
   const queryClient = new QueryClient();
@@ -14,10 +16,21 @@ describe("recipe details page", async () => {
   it("should render component", async () => {
     render(
       <QueryClientProvider client={queryClient}>
-        <Router>
-          <RecipeDetailPage />
-        </Router>
+        <UserContextProvider testUser="test">
+          <Router>
+            <RecipeDetailPage />
+          </Router>
+        </UserContextProvider>
       </QueryClientProvider>
+    );
+    server.use(
+      http.get(
+        import.meta.env.VITE_server_recipeDetails,
+        () => {
+          return HttpResponse.json([]);
+        },
+        { once: true }
+      )
     );
     await waitFor(() => {
       expect(
